9776c4289130050e24985513cd4adc9c2137c649
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-27 13:43:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 
 dotenv.load_dotenv("local.env")
 
-api_id = int(os.environ.get("API_ID") or 6)
-api_hash = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+api_id = 11573285
+api_hash = "f2cc3fdc32197c8fbaae9d0bf69d2033"
+BOT_TOKEN = "6146834668:AAEDP6j0sB_DXwzUQhutfBS41V4bbEsj0Qk"
 FORCE_JOIN_CHAT_ID = int(os.environ.get('FORCE_JOIN_CHAT_ID', 1))
 CHANNEL_LINK = os.environ.get('CHANNEL_LINK')
 LOG_STRING_SESSION = int(os.environ.get('LOG_STRING_SESSION', 1))
