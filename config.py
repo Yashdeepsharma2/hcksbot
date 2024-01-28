1596d07523872f96ce8741f84f30a1bbeb5a1090
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-28 13:57:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ api_hash = "f2cc3fdc32197c8fbaae9d0bf69d2033"
 BOT_TOKEN = "6443237594:AAE1k8Ju2PGci7YgrnI0r8rlBWb9FbwM2DA"
 FORCE_JOIN_CHAT_ID = int(os.environ.get("FORCE_JOIN_CHAT_ID", 1))
 CHANNEL_LINK = os.environ.get("CHANNEL_LINK")
-LOG_STRING_SESSION = int(os.environ.get("LOG_STRING_SESSION", None))
+LOG_STRING_SESSION = int(os.environ.get("LOG_STRING_SESSION", 5591734243))
