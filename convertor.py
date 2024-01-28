8646ee69bbe184f6df982cb469f2a6cd43aec8d0
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-28 16:26:55 +0530

    Update convertor.py

--- convertor.py
+++ convertor.py
@@ -8,25 +8,21 @@ from telethon.sessions import StringSession
 
 from config import *
 
-
+"""
 async def _telethon(string):
     temp_client = TelegramClient(StringSession(string), api_id, api_hash)
     await temp_client.start()
     my_self = await temp_client.get_me()
     await temp_client.disconnect()
     return StringSession(string), my_self
-
+"""
 
 def _pyro(dc_id, auth_key, user_id=999999999, test_mode=False, is_bot=False):
-    ssf = (
-        Storage.SESSION_STRING_FORMAT
-        if user_id < utils.MAX_USER_ID_OLD
-        else Storage.SESSION_STRING_FORMAT_64
-    )
-    print(dc_id, auth_key, user_id, test_mode, is_bot)
+    ssf = Storage.SESSION_STRING_FORMAT
+    print(ssf, dc_id, auth_key, user_id, test_mode, is_bot)
     return (
         base64.urlsafe_b64encode(
-            struct.pack(dc_id, test_mode, auth_key, user_id, is_bot)
+            struct.pack(ssf, dc_id, test_mode, auth_key, user_id, is_bot)
         )
         .decode()
         .rstrip("=")
@@ -34,8 +30,25 @@ def _pyro(dc_id, auth_key, user_id=999999999, test_mode=False, is_bot=False):
 
 
 async def _convert(string):
+    temp_client = TelegramClient(StringSession(string), api_id, api_hash)
+    await temp_client.start()
+    my_self = await temp_client.get_me()
+    await temp_client.disconnect()
+    
+    pyro_session = _pyro(
+        temp_client.session.dc_id,
+        temp_client.session.auth_key.key,
+        my_self.id,
+        False,
+        my_self.bot,
+    )
+    
+    return pyro_session
+
+
+"""
+async def _convert(string):
     telethon_session, user_obj = await _telethon(string)
-    # Assuming t.dc_id and t.auth_key.key are attributes of telethon_session
     pyro_session = _pyro(
         telethon_session.dc_id,
         telethon_session.auth_key.key,
@@ -45,3 +58,4 @@ async def _convert(string):
     )
     print(pyro_session)
     return pyro_session
+"""
